c6e4f0403a19112edf44d3b046f526118b80ae5c
Ishan Subedi <ikushum@gmail.com>
2025-03-04 13:27:25 +0545

    create global snackbar component

---
+++ src/components/Layouts/TheAppSnackbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-snackbar
+    v-model="snackbarStore.snackbar.isOpen"
+    :color="snackbarStore.snackbar.color"
+  >
+    <span>
+      {{ snackbarStore.snackbar.message }}
+    </span>
+
+    <template #actions>
+      <v-btn
+        icon
+        small
+        dark
+        @click="snackbarStore.snackbar.isOpen = false"
+      >
+        <v-icon small>
+          {{ mdiClose }}
+        </v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script setup lang="ts">
+import { mdiClose } from "@mdi/js";
+import { useSnackbarStore } from "@/stores/snackbar";
+
+const snackbarStore = useSnackbarStore();
+</script>
