377027782d1653fdf21f84625ffca5e4eefafd8c
Ishan Subedi <ikushum@gmail.com>
2025-03-04 13:26:39 +0545

    display no blog placeholder

---
+++ src/components/Blog/NoBlogPlaceholder.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container
+    class="py-16 fill-height d-flex flex-column align-center justify-center"
+  >
+    <div class="text-h2 bg-grey-lighten-3 pa-10 mb-10 rounded-circle">
+      <v-icon color="grey">
+        {{ mdiFolderOutline }}
+      </v-icon>
+    </div>
+
+    <div class="mb-5 text-center">
+      <h3 class="text-h4 mb-2 font-weight-bold">
+        You haven't posted any blogs yet
+      </h3>
+
+      <p class="text-grey">
+        Start creating blogs by clicking on "Create Blog"
+      </p>
+    </div>
+
+    <v-btn
+      flat
+      color="primary"
+      class="px-10 mb-10"
+      @click="router.push({ path: '/create' })"
+    >
+      Create New
+    </v-btn>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import { mdiFolderOutline } from "@mdi/js";
+
+const router = useRouter();</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,40 +1,44 @@
 <template>
-  <v-container>
-    <h3 class="text-h5 font-weight-bold my-5">
-      Latest Blog
-    </h3>
+  <template v-if="!blogs.length">
+    <NoBlogPlaceholder />
+  </template>
 
-    <div class="mb-10">
-      <LargeBlogCard
-        :blog="latestBlog"
-      />
-    </div>
+  <template v-else-if="blogs.length">
+    <v-container>
+      <h3 class="text-h5 font-weight-bold my-5">
+        Latest Blog
+      </h3>
 
-    <h3 class="text-h6 font-weight-bold my-5">
-      More Blogs
-    </h3>
+      <div class="mb-10">
+        <LargeBlogCard :blog="latestBlog" />
+      </div>
 
-    <v-row class="mb-10">
-      <v-col
-        v-for="(blog) in popularBlogs"
-        :key="blog.id"
-        xs="12"
-        sm="6"
-        md="4"
-      >
-        <SmallBlogCard
-          :blog="blog"
-        />
-      </v-col>
-    </v-row>
-  </v-container>
+      <template v-if="blogs.length > 1">
+        <h3 class="text-h6 font-weight-bold my-5">
+          More Blogs
+        </h3>
+
+        <v-row class="mb-10">
+          <v-col
+            v-for="blog in popularBlogs"
+            :key="blog.id"
+            cols="12"
+            md="4"
+          >
+            <SmallBlogCard :blog="blog" />
+          </v-col>
+        </v-row>
+      </template>
+    </v-container>
+  </template>
 </template>
 
 <script setup lang="ts">
-import { loadBlogs } from '@/utils/blog'
+import { useBlogStore } from '@/stores/blog'
 
-const blogs = loadBlogs()
+const blogStore = useBlogStore()
 
-const latestBlog = computed(() => blogs[0])
-const popularBlogs = computed(() => [...blogs].slice(1))
+const blogs = computed(() => blogStore.blogs)
+const latestBlog = computed(() => blogs.value[0]);
+const popularBlogs = computed(() => [...blogs.value].slice(1));
 </script>
